6d801c9c53cb869489019570cc992ae3eeec27f4
guolanren <lanrenguo@gmail.com>
2019-12-23 18:46:02 +0800

    spring-security-cors

---
+++ spring-security-cors/README.md
@@ -0,0 +1,5 @@
+# spring-security-cors
+
+## 概述
+
+spring-security-cors 概述...
---
+++ spring-security-cors/src/main/java/name/guolanren/config/WebSecurityConfiguration.java
@@ -0,0 +1,52 @@
+package name.guolanren.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+/**
+ * @author guolanren
+ */
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers("/index").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .and()
+            .rememberMe()
+                .userDetailsService(userDetailsService)
+                .and()
+            .cors()
+                .and()
+            .csrf().disable();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://found.guolanren.name"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
