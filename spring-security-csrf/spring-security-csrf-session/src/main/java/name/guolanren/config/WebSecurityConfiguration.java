086e49181aa3b46c4ab32cc9dca02ca7224fd1d8
guolanren <lanrenguo@gmail.com>
2020-04-30 16:00:01 +0800

    Update WebSecurityConfiguration.java

--- spring-security-csrf/spring-security-csrf-session/src/main/java/name/guolanren/config/WebSecurityConfiguration.java
+++ spring-security-csrf/spring-security-csrf-session/src/main/java/name/guolanren/config/WebSecurityConfiguration.java
@@ -3,7 +3,6 @@ package name.guolanren.config;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 /**
  * @author guolanren
